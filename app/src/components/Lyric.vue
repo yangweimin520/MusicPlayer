9517ccdfc0b5d94d1e0f292dd8b0a5f2ce63ebeb
Weimin Yang <weiyang@microsoft.com>
2019-05-31 17:32:16 +0800

    adfd

--- app/src/components/Lyric.vue
+++ app/src/components/Lyric.vue
@@ -5,47 +5,78 @@
 </template>
 
 <script>
-import Base64 from '../utils/base64'
+import Base64 from "../utils/base64";
 export default {
-  props: ['currentTime', 'songid'],
-  data () {
+  props: ["currentTime", "songid"],
+  data() {
     return {
       lyric: null
-    }
+    };
   },
   components: {},
   methods: {},
   computed: {
     currentLyric: function() {
-      if(this.lyric !== null) {
+      if (this.lyric !== null) {
         let that = this;
         let pastLyric = [];
         let i = 0;
-        Object.keys(this.lyric).forEach(function (key) {
-          if(key.split(':')
-            .reduce((a, b) => parseInt(a) * 60 * 100 + b.split('.').reduce((a, b) => parseInt(a) * 100 + parseInt(b))) -120 <= that.currentTimeStamp) {
-              if(that.lyric[key] !== '\n') pastLyric.push(that.lyric[key])
-            } else if(i <= 1 && that.lyric[key] != '\n') {
-              pastLyric.push(that.lyric[key]);
-              i++;
-            }
-        })
+        Object.keys(this.lyric).forEach(function(key) {
+          if (
+            key
+              .split(":")
+              .reduce(
+                (a, b) =>
+                  parseInt(a) * 60 * 100 +
+                  b.split(".").reduce((a, b) => parseInt(a) * 100 + parseInt(b))
+              ) -
+              120 <=
+            that.currentTimeStamp
+          ) {
+            if (that.lyric[key] !== "\n") pastLyric.push(that.lyric[key]);
+          } else if (i <= 1 && that.lyric[key] != "\n") {
+            pastLyric.push(that.lyric[key]);
+            i++;
+          }
+        });
       }
       return pastLyric.slice(pastLyric.length - 4, pastLyric.length - 1);
     }
   },
   watch: {
     songid: function(id) {
-      this.$store.dispatch('getLyric', id)
-      .then((response) => {
-        this.lyric = Base64
-          .decode(response.data.lyric)
-      })
+      this.$store.dispatch("getLyric", id).then(response => {
+        this.lyric = Base64.decode(response.data.lyric)
+          .split("[")
+          .slice(5)
+          .map(str => {
+            let t = str.split("]");
+            return { [t[0]]: t[1] };
+          })
+          .reduce((a, b) => {
+            return { ...a, ...b };
+          });
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
+#lyric {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+  flex-grow: 1;
+}
+.lyric-item {
+  text-align: center;
+}
+.lyric-item {
+  :first-child, :last-child {
+    font-size: 14px;
+    color: #4d4d4d;
+  }
+}
 </style>
